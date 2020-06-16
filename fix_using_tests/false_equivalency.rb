5ff640f7de14228f680b37ea1d23d9fe892ba972
Kiri Um <kirirotha@gmail.com>
2020-06-16 16:36:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,5 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -17,6 +18,7 @@ def selection(num)
      "HAM HAM HAM IN MY TUMMY"
   end
 end
+binding.pry
 
 def runner
   prompt_user
